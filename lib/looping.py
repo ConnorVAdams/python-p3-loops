facf1acf1431b83612f5b636d6c3f3434fc28846
Connor Adams <connorvadams@gmail.com>
2023-10-23 11:12:30 -0600

    Submits lab.

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i-= 1
+    print('Happy New Year!')
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num * 2 for num in int_list]
+
+ints = square_integers([1, 2, 3, 4, 5])
+print(ints)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 0
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+        i += 1
+
+fizzbuzz()
